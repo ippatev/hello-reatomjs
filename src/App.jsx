88f7e67fe8dd1414c9dac81027c3fc3d4345d9d2
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2022-04-22 05:22:45 +0900

    Add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import { createStore } from '@reatom/core'
+import { createStore } from "@reatom/core"
 import { reatomContext } from "@reatom/react"
+import Form from "./components/Form"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const store = createStore()
 
   return (
     <div className="App">
+      <reatomContext.Provider value={store}>
+        <Form></Form>
+      </reatomContext.Provider>
     </div>
   )
 }
