46d7dd1914731e5a41513cd58cfab62b81566319
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2022-04-22 22:11:58 +0900

    Add AgeAtom

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,7 +16,20 @@ const nameAtom = createAtom(
       return state = payload
     })
 
-    schedule(() => console.log(`STATE: ${state}`))
+    schedule(() => console.log(`NameAtom STATE: ${state}`))
+
+    return state
+  }
+)
+
+const ageAtom = createAtom(
+  { setAge: (value) => value },
+  ({onAction, schedule}, state = 0) => {
+    onAction("setAge", (payload) => {
+      return state = payload
+    })
+
+    schedule(() => console.log(`AgeAtom STATE: ${state}`))
 
     return state
   }
@@ -24,13 +37,20 @@ const nameAtom = createAtom(
 
 function Form() {
   const [name, {setName}] = useAtom(nameAtom)
+  const [age, {setAge}] = useAtom(ageAtom)
+
   const handleSetName = useAction(e => setName(e.target.value)) 
+  const handleSetAge = useAction(e => setAge(e.target.value))
 
   
   return (
     <form>
       <label htmlFor="name">Enter your name</label>
       <input id="name" value={name} onChange={handleSetName} />
+
+
+      <label htmlFor="age">Enter your age</label>
+      <input type="number" id="age" value={age} onChange={handleSetAge} />
     </form>
   )
 }
